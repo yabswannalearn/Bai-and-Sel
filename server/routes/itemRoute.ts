73f713241e929b0e5b7fcaca3b377e1d0b5ba3c4
reinael123 <reinaelyabut1234@gmail.com>
2025-09-21 09:34:28 +0800

    feat: adding constraints to auth (register)

--- server/routes/itemRoute.ts
+++ server/routes/itemRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import { getItems, postItems} from "../controller/itemsController"
+import { authMiddleware } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/items", getItems)
-router.post("/items", postItems)
+router.post("/items", authMiddleware, postItems)
 
 export default router;
